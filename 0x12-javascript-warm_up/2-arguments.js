a999d1f9e0d61784315e55d9dba07726f5b80b7e
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-08-25 12:14:11 -0700

    Tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const count = process.argv.length;
-console.log(count === 2 ? ' No argument' : count == 3 ? 'Argument foundt' : 'Argument Found');
+console.log(count === 2 ? ' No argument' : count === 3 ? 'Argument foundt' : 'Argument Found');
