ab267ec5ad7e2280c1c417a21f22c187cb2c5914
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-08-31 16:15:53 -0700

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Check if a URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
 # Use curl to send a request and display the size of the response body in bytes
 curl -s "$1" | wc -c
 
