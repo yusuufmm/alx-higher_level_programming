980d76ba09bb927fe7a4b77b3514470ca8c77384
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-08-31 16:52:54 -0700

    python network

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
+def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers (list): List of integers to find a peak in.
+    Returns:
+        int or None: Peak element of the list, or None if the list is empty.
+    """
+    size = len(list_of_integers)
+
+    if size == 0:
+        return None
+
+    mid = size // 2
+    mid_e = size
+
+    while True:
+        mid_e = mid_e // 2
+
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
