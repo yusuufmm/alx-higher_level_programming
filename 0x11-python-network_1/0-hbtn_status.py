d1bedcfe049b9d5b40a1e465f511ad7fd82172b3
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-09-01 12:07:16 -0700

    0 hbtn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,17 +9,9 @@ try:
     with urllib.request.urlopen(url) as response:
         # Read the response content into the 'content' variable
         content = response.read()
-
-        # Print information about the response
         print("Body response:")
-
-        # Print the type of 'content' (bytes)
         print("    - type:", type(content))
-
-        # Print the raw content (bytes)
         print("    - content:", content)
-
-        # Print the content as UTF-8 (decoded bytes)
         print("    - utf8 content:", content.decode("utf-8"))
 except Exception as e:
     # Handle any exceptions that may occur during the request
