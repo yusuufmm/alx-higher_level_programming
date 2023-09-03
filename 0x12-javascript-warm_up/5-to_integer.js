ed717080bfd9d96fdbdbedb6dec79d3f16caf8a7
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-09-03 16:54:22 -0700

    Js tasks

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,5 @@
-#!/usr/bin/nod
-e
+#!/usr/bin/node
 /* script that prints My number: <first argument converted in integer> */
-const arg = process.argv[2];
 const num = Math.floor(Number(process.argv[2]));
+
 console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
