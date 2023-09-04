e588cf41f347868ab516f641656eda4cfd4e1f17
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-09-04 16:30:25 -0700

    adv tasks

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 /* script that prints the addition of 2 */
+
 function add (a, b) {
   return a + b;
 }
-const arg1 = parseInt(process.argv[2]);
 
-const arg2 = parseInt(process.argv[3]);
-
-if (!isNaN(arg1) && !isNaN(arg2)) {
-  console.log(add(arg1, arg2));
-} else {
-  console.log('Please provide two valid integers.');
-}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
