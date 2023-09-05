2ed52afd3d19f81b5b6a8247e0080191dc62e651
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-09-05 15:19:38 -0700

    tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode(value).encode("ascii")
+    data = urllib.parse.urlencode({'email': email})
     data = data.encode('utf-8')
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
