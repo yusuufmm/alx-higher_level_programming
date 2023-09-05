0cf9e27bb55494c674ef568fee065815e1354024
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-09-05 06:08:22 -0700

    Tasks JS

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+/* empty class Rectangle that defines a rectangle */
+module.exports = class Rectangle {};
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*  class Rectangle that defines a rectangle: */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    /// width
+    this.width = w;
+    // height
+    this.height = h;
+  }
+};
