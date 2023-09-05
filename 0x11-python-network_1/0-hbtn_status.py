84e6f09508eabe3c55b72c09c2e4022e47bcd8e5
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-09-05 15:07:13 -0700

    nt py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,6 @@ import urllib.request
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
-
-    # Send a GET request to the URL and open a connection
-    with urllib.request.urlopen(request) as response:
-        # Read the response content into the 'content' variable
-        with urllib.request.urlopen(request) as response:
-
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL, sends a request to the URL and displays the body of the response."""
+""" Python script that takes in a URL, sends a request to the URL ."""
 import sys
 import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-
-     u = requests.get(url)
-     if u.status_code >= 400:
+    u = requests.get(url)
+    if u.status_code >= 400:
         print("Error code: {}".format(u.status_code))
-     else:
+    else:
         print(u.text)
