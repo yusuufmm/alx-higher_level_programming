8b27986b7f11adc7e241d5132037b6219451bd11
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-09-05 15:14:00 -0700

    tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
- """  Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id """
+"""Python script that takes your GitHub credentials"""
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-
     r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
-
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
-     r = requests.get(url)
+    r = requests.get(url)
     commits = r.json()
     try:
         for i in range(10):
