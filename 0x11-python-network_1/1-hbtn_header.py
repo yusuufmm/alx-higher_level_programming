aebada3921fe7ade5a69dbc071f63cd544c0ebf2
YUSUF MM <109472955+yusuufmm@users.noreply.github.com>
2023-11-04 14:36:47 +0100

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Python script that takes in a URL, sends a request to the URL. """
-import sys
-import urllib.request
-if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        print(dict(response.headers).get("X-Request-Id"))
